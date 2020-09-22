4531415da5665aeb2ad8058232235e480e4787d6
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-09-22 12:10:12 +0100

    Minify and inline JS & CSS for prod

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,20 @@ const markdownReveal = require("markdown-it-revealjs");
 const markdownItTitle = require("markdown-it-title");
 const markdownItDecorate = require("markdown-it-decorate");
 const CleanCSS = require("clean-css");
+const { minify } = require("terser");
 
 module.exports = (config) => {
   config.addPassthroughCopy("src/assets");
 
   config.addFilter("cssmin", (code) => new CleanCSS({}).minify(code).styles);
+  config.addNunjucksAsyncFilter("jsmin", (code, cb) =>
+    minify(code)
+      .then((minified) => cb(null, minified.code))
+      .catch((err) => {
+        console.error("Terser error: ", err);
+        cb(null, code);
+      })
+  );
 
   const md = markdownIt({
     html: true, // passthrough raw html in md files
