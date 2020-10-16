33406a195c5da13f38b904c2791178d4f79ecce5
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-10-16 18:53:45 +0100

    Fix markdown slide logic
    It now correctly loops when you go back from slide 0

--- markdown-it-slides.js
+++ markdown-it-slides.js
@@ -1,29 +1,19 @@
 function slidify(md) {
   md.core.ruler.push("slidify", function (state) {
-    let count = 0;
+    const dividers = state.tokens.filter((t) => t.type === "hr");
+    const count = dividers.length;
 
-    state.tokens.forEach((token, index) => {
-      if (token.type !== "hr") return;
-
-      count++;
+    dividers.forEach((token, index) => {
+      const prevSlide = index - 1 < 0 ? count - 1 : index - 1;
+      const nextSlide = index + 1;
       const replacement = new state.Token("html_block", "", 0);
       replacement.content = `
-          <nav>
-            <a href="#${count - 2}" aria-label="Previous">
-              <svg width="36" height="36" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
-              </svg>
-            </a>
-            <a href="#${count}" aria-label="Next">
-              <svg width="36" height="36" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-              </svg>
-            </a>
-          </nav>
-        </section>
-        <section id="${count}" class="slide">
+  ${nav(prevSlide, nextSlide)}
+</section>
+<section id="${nextSlide}" class="slide">
       `;
-      state.tokens.splice(index, 1, replacement);
+      const originalIndex = state.tokens.indexOf(token);
+      state.tokens.splice(originalIndex, 1, replacement);
     });
 
     const open = new state.Token("html_block", "", 0);
@@ -31,9 +21,29 @@ function slidify(md) {
     state.tokens.unshift(open);
 
     const close = new state.Token("html_block", "", 0);
-    close.content = "</section>";
+    close.content = `
+  ${nav(count - 2, 0)}
+</section>
+    `;
     state.tokens.push(close);
   });
+}
+
+function nav(prev, next) {
+  return `
+  <nav>
+    <a href="#${prev}" class="button" aria-label="Previous">
+      <svg width="36" height="36" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+      </svg>
+    </a>
+    <a href="#${next}" class="button" aria-label="Next">
+      <svg width="36" height="36" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+      </svg>
+    </a>
+  </nav>
+  `;
 }
 
 module.exports = slidify;
